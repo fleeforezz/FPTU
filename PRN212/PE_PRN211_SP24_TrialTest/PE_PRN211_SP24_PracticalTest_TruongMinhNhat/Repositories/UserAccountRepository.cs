e8a568ef0d7bcdbd0cfd5be0d836decc87f202dd
Trương Minh Nhật <fleeforezz@gmail.com>
2024-07-19 14:13:39 +0700

    Update Trial Test

---
+++ PRN212/PE_PRN211_SP24_TrialTest/PE_PRN211_SP24_PracticalTest_TruongMinhNhat/Repositories/UserAccountRepository.cs
@@ -0,0 +1,22 @@
+﻿using Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class UserAccountRepository
+    {
+        private BookManagementDbContext _context;
+        public UserAccountRepository()
+        {
+            _context = new BookManagementDbContext();
+        }
+        public UserAccount? Login(String Email, String Password)
+        {
+            return _context.UserAccounts.Where(x => x.Email == Email && x.Password == Password).FirstOrDefault();
+        }
+    }
+}
---
+++ PRN212/PE_PRN211_SP24_TrialTest/PE_PRN211_SP24_PracticalTest_TruongMinhNhat/Services/UserAccountService.cs
@@ -0,0 +1,23 @@
+﻿using Repositories;
+using Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class UserAccountService
+    {
+        private UserAccountRepository _repository;
+        public UserAccountService()
+        {
+            _repository = new UserAccountRepository();
+        }
+        public UserAccount? Login(String Email, String Password)
+        {
+            return _repository.Login(Email, Password);
+        }
+    }
+}
