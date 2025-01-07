7f6f55b4f46cc52f70d8c256cf43e3cb945429df
Trương Minh Nhật <fleeforezz@gmail.com>
2025-01-07 14:32:10 +0700

    Update

--- PRO/HelloWorld/src/main.java
+++ PRO/HelloWorld/src/main.java
@@ -15,6 +15,15 @@ public class main {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        int refult = calcNum(12, 12);
+        
+        System.out.println("Result: " + refult);
     }
     
+    public static int calcNum(int x, int y) {
+        int sum = 0;
+        
+        return sum = x + y;
+    }
 }
