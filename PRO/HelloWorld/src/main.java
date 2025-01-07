0ffc8d3efe06033f98f989c5ff101c81f85b9c57
Trương Minh Nhật <fleeforezz@gmail.com>
2025-01-07 14:37:15 +0700

    Update main.java

--- PRO/HelloWorld/src/main.java
+++ PRO/HelloWorld/src/main.java
@@ -1,13 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author jso
- */
 public class main {
 
     /**
@@ -23,7 +13,6 @@ public class main {
     
     public static int calcNum(int x, int y) {
         int sum = 0;
-        
         return sum = x + y;
     }
 }
