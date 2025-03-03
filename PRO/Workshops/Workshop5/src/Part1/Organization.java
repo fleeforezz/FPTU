911d8a11fb76d2fed3025645dd7f48affe662c56
Trương Minh Nhật <fleeforezz@gmail.com>
2025-03-03 11:12:30 +0700

    Update

---
+++ PRO/Workshops/Workshop5/src/Part1/Organization.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Part1;
+
+/**
+ *
+ * @author jso
+ */
+public abstract class Organization {
+    protected int size;
+
+    public Organization() {
+    }
+
+    public Organization(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+    
+    public abstract void communicateByTool();
+
+    @Override
+    public String toString() {
+        return "Organization{" + "size=" + size + '}';
+    }
+    
+    
+}
