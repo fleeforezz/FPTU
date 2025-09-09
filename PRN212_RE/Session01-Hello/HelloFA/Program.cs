9c8b790a5b59447c469b1eb066761a629bebb4d1
Trương Minh Nhật <fleeforezz@gmail.com>
2025-09-09 09:10:08 +0700

    Update

--- PRN212_RE/Session01-Hello/HelloFA/Program.cs
+++ PRN212_RE/Session01-Hello/HelloFA/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World my name is Nhat");
+        Console.ReadLine();
     }
 }
