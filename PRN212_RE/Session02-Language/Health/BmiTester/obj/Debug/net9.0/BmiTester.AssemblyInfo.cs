a08640c93bd803506af4d8c256f1773f40be1655
Trương Minh Nhật <fleeforezz@gmail.com>
2025-09-23 07:58:57 +0700

    Update

--- PRN212_RE/Session02-Language/Health/BmiTester/obj/Debug/net9.0/BmiTester.AssemblyInfo.cs
+++ PRN212_RE/Session02-Language/Health/BmiTester/obj/Debug/net9.0/BmiTester.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BmiTester")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f580499f907d101a9259d93e7d4472c1b1d95951")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+057837c318217f523b81c97d32d05ec67921a667")]
 [assembly: System.Reflection.AssemblyProductAttribute("BmiTester")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BmiTester")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PRN212_RE/Session02-Language/HealthV4/BmiTester/obj/Debug/net9.0/BmiTester.AssemblyInfo.cs
+++ PRN212_RE/Session02-Language/HealthV4/BmiTester/obj/Debug/net9.0/BmiTester.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BmiTester")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f530326dc3bb4e7bcac2eb1bec423d9e32f09de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+057837c318217f523b81c97d32d05ec67921a667")]
 [assembly: System.Reflection.AssemblyProductAttribute("BmiTester")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BmiTester")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
